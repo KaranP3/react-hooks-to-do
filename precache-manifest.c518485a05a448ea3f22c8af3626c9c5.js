6ddd066eb6a9ba6dca7bcaa0f5d8ee250f49ea19
KaranP3 <karanpillai93@gmail.com>
2019-07-05 00:31:01 +0530

    Updates

---
+++ precache-manifest.c518485a05a448ea3f22c8af3626c9c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b6e45ec16b1441be77bd26b34b3c95",
+    "url": "/react-hooks-to-do/index.html"
+  },
+  {
+    "revision": "d2696c06ca1e4ae72c27",
+    "url": "/react-hooks-to-do/static/css/main.3881d2d4.chunk.css"
+  },
+  {
+    "revision": "2188eb62c38f29023e30",
+    "url": "/react-hooks-to-do/static/js/2.0fbf8cbb.chunk.js"
+  },
+  {
+    "revision": "d2696c06ca1e4ae72c27",
+    "url": "/react-hooks-to-do/static/js/main.3b80a71f.chunk.js"
+  },
+  {
+    "revision": "f5d115c1e06adaa16bbf",
+    "url": "/react-hooks-to-do/static/js/runtime~main.333e3b80.js"
+  }
+]);
